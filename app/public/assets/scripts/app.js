17c8715e97e8eb15fef57efff2612d4daf1bf40c
Bradley Knutson <bradley@bknutson.com>
2020-01-07 14:16:22 -0700

    Added post request functionality

--- app/public/assets/scripts/app.js
+++ app/public/assets/scripts/app.js
@@ -1,4 +1,29 @@
-$('#submit').on('click', (e) => {
-    e.preventDefault();
-    console.log($('#q1').val());
+$(document).ready(() => {
+    $('#submit').on('click', (e) => {
+        e.preventDefault();
+        let name = $('#name').val();
+        let photo = $('#photo').val();
+        let answersArr = [$('#q1').val(), $('#q2').val(), $('#q3').val(), $('#q4').val(), $('#q5').val(), $('#q6').val(), $('#q7').val(), $('#q8').val(), $('#q9').val(), $('#q10').val()];
+        if(!answersArr.includes('') && name !== '' && photo !== ''){
+            $('#name, #photo, #q1, #q2, #q3, #q4, #q5, #q6, #q7, #q8, #q9, #q10').val('');
+            let friendObj = {
+                name: name,
+                photo: photo,
+                scores: answersArr
+            }
+            checkFriend(friendObj);
+        }else{
+            //Show missing answers
+            console.log('stopit');
+        }
+    });
+
+
 });
+
+function checkFriend(friendObj){
+    $.post(`/api/friends`, friendObj, (data, status) => {
+        //Return closest matched friend as data
+        console.log(data, status);
+    });
+}
