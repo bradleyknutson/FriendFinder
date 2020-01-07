49986c96170dcd73b5e0c0dd9c9745876dd89972
Bradley Knutson <bradley@bknutson.com>
2020-01-07 11:05:16 -0700

    Added functionality to find closest friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,11 +3,27 @@ let friends = [
         name: "Bradley",
         photo: "https://media.licdn.com/dms/image/C4E03AQHG90y9k0vkKA/profile-displayphoto-shrink_200_200/0?e=1583971200&v=beta&t=yZIN7XC5VwgDAyBjNIUrUOzASuShOjk0reVKhmZpJSc",
         scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Steven",
+        photo: "https://thingamajig.com",
+        scores: [1,3,5,5,4,3,6,8,4,1]
     }
 ];
 
 function findFriend(user){
-    return friends[0];
+    let friendScoreDifference = 51;
+    let friend;
+    friends.forEach(person => {
+        let personScoreDifference = 0;
+        for(let i = 0; i<person.scores.length; i++) {
+            personScoreDifference += Math.abs(person.scores[i] - user.scores[i]);
+        }
+        if(personScoreDifference < friendScoreDifference){
+            friend = person;
+        }
+    });
+    return friend;
 }
 
 module.exports = {
