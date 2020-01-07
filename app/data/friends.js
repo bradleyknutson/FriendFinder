16512c3f83c2cab7ca99c08db73d05c945df3d75
Bradley Knutson <bradley@bknutson.com>
2020-01-07 16:36:17 -0700

    Removed base friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,4 @@
-let friends = [
-    {
-        name: "Bradley",
-        photo: "https://media.licdn.com/dms/image/C4E03AQHG90y9k0vkKA/profile-displayphoto-shrink_200_200/0?e=1583971200&v=beta&t=yZIN7XC5VwgDAyBjNIUrUOzASuShOjk0reVKhmZpJSc",
-        scores: [5,1,4,4,5,1,2,5,4,1]
-    },
-    {
-        name: "Steven",
-        photo: "https://thingamajig.com",
-        scores: [1,3,5,5,4,3,6,8,4,1]
-    }
-];
+let friends = [];
 
 function findFriend(user){
     let friendScoreDifference = 51;
